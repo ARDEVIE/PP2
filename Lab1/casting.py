679fdb0ac142823a2633f207278213cdf61f8e48
ARDEVIE <almasbekov.damir777@gmail.com>
2025-01-18 19:39:05 +0500

    casting

---
+++ Lab1/casting.py
@@ -0,0 +1,11 @@
+x = int(1)   # x will be 1
+y = int(1.1) # y will be 1
+z = int("2") # z will be 2
+
+x = float(1)     # x will be 1.0
+y = float(1.5)   # y will be 1.5
+z = float("1")   # z will be 1.0
+
+x = str("s1") # x will be 's1'
+y = str(1)    # y will be '1'
+z = str(1.0)  # z will be '1.0'
