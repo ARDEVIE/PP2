a33e5f1fb76fb56f6e699914aa51f4b9f31891a3
ARDEVIE <almasbekov.damir777@gmail.com>
2025-01-18 19:17:09 +0500

    update

---
+++ Lab1/comment.py
@@ -0,0 +1,13 @@
+#Placement
+    #This is a comment
+    print("Hello, World!")
+    print("Hello, World!") #This is a comment
+
+#Prevent from executing 
+    #print("Hello, World!")
+
+#Multiline Comments
+    #This is a comment
+    #written in
+    #more than just one line
+    print("Hello, World!")
