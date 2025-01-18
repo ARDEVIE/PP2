42b1a44a7e3f06704a440e1a1c937210d899d14d
ARDEVIE <almasbekov.damir777@gmail.com>
2025-01-18 19:26:04 +0500

    data types

---
+++ Lab1/datatypes.py
@@ -0,0 +1,27 @@
+x = 5
+print(type(x))
+#int
+
+x = "Hello World"
+print(type(x))
+#str
+
+x = 20.5
+print(type(x))
+#float
+
+x = ["book", "pen", "paper"]
+print(type(x))
+#list
+
+x = ("book", "pen", "paper")
+print(type(x))
+#tuple
+
+x = {"name" : "Bob", "age" : 18}
+print(type(x))
+#dict
+
+x = True
+print(type(x))
+#bool
