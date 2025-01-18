37cb4fab8eb77fd466844b91c776c24584d4ecb4
ARDEVIE <almasbekov.damir777@gmail.com>
2025-01-18 19:00:47 +0500

    Lab1

---
+++ Lab1/syntax.py
@@ -0,0 +1,17 @@
+#Indentation
+if 5 > 2:
+  print("Five is greater than two!") #The number of spaces, it has to be at least one.
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!")
+ print("Five is greater than two!") # the same number of spaces in the same block of code 
+
+#Variables
+x = 5
+y = "Hello, World!"
+
