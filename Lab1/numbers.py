5d8f8ce1730cc4583700d6c34fae39f8e87ceceb
ARDEVIE <almasbekov.damir777@gmail.com>
2025-01-18 19:34:37 +0500

    numbers

---
+++ Lab1/numbers.py
@@ -0,0 +1,17 @@
+#Types
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Type Conversion
+x = 1
+x = float(x)
+
+x = 1.2
+x = int(x)
+
+x = 1
+x = complex(x)
