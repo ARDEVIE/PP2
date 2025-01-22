6a0246341451ff0677bfb28816a4ce5c99cea1ee
ARDEVIE <almasbekov.damir777@gmail.com>
2025-01-22 10:35:26 +0500

    booleans

---
+++ Lab2/booleans.py
@@ -0,0 +1,6 @@
+print(5 > 3)  # Output: True
+print(10 > 9) # Output: True
+print(10 == 9) # Output: False
+print(10 < 9) # Output: False
+print(bool("abc")) # Output: True
+print(bool(0)) # Output: True
